fe3758a12a3a254e5f605247dac11e94c22968dd
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2023-06-06 21:02:11 -0300

    Components and css

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  styles: [`
+    p {
+      color: blue;
+    }
+  `]
 })
 export class ServerComponent {
 
