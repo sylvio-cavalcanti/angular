59d2e0b446ff01e491c4cb3620863623303f6ffd
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2023-06-07 15:59:06 -0300

    Databinding examples

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created...';
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true
+      console.log('allowNewServer: True');
+    }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created.';
+  }
 }
