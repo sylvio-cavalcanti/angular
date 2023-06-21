4faa0ec5880c8b3d9fbad8d3340d6879cd457736
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2023-06-21 12:15:26 -0300

    ngStyle directive

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -13,7 +13,17 @@ export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = 'offline';
 
+  // Note: The constructor is a built-in method that is called when the component is created
+  constructor() {
+    // Generates a random number between 0 and 1 so we have diferente status for demonstration purposes
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   getServerStatus () {
     return this.serverStatus
+  }
+
+  getColor () {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
